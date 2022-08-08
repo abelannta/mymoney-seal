84387f663e8f3c758ecd328097229ed70382fe39
Graha Yohanes Napitupulu <luftschols@gmail.com>
2022-08-08 23:55:38 +0700

    update css

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,5 +1,5 @@
 import { React, useState } from "react";
-import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import { Container, Form, Button} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../login/login.css"
 import FormInput from "../../component/login/formInput"
@@ -43,11 +43,13 @@ function Login() {
     };
 
     return (
-        <Container className="LoginPage">
+        <Container className="LoginPage" fluid="true">
             <Form onSubmit={handleSubmit}>
                 <img src={LOGOMYMONEY} className="brand-image" alt="Logo MyMoney" />
+                <div className="title">
                 <h1>Welcome back</h1>
                 <p>Welcome back! Please enter your details</p>
+                </div>
                 {inputs.map((input) => (
                     <FormInput
                         key={input.id}
@@ -57,27 +59,24 @@ function Login() {
                     />
                 ))}
 
-<Row className="optionLogin">
-    <Col>
                 {['checkbox'].map((type) => (
                     <div key={`default-${type}`} className="mb-3">
                         <Form.Check
                             type={type}
-                            id={`default-${type}`}
-                            label={`Remember for 30 days?`}
+                            id={`checkbox`}
+                            label={`Remember for 30 Days`}
                         />
-                        
+                        <Link style={styleLink} to="/" className="forgotPass">Forgot password?</Link>
                     </div>
                 ))}
-    </Col>
-    <Col className="forgotPass">
-       <Link style={styleLink} to="/">Forgot password?</Link>
-    </Col>
-</Row>
+
+
 
                 <Button>Sign in</Button>
-
+                
+                <div className="footer">
                 <p>Dont have an account? <Link style={styleLink} to="/">Sign up for free</Link></p>
+                </div>
             </Form>
 
         </Container>
