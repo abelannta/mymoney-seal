cb3d0a07e5fe1108b725464de0e8a4363cc07046
Graha Yohanes Napitupulu <luftschols@gmail.com>
2022-08-09 01:21:54 +0700

    register 90% done

---
+++ src/pages/register/register.js
@@ -0,0 +1,81 @@
+import { React, useState } from "react";
+import { Container, Form, Button} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../register/register.css"
+import FormInput from "../../component/login/formInput"
+import LOGOMYMONEY from "../../images/login/myMoneyLogo.png"
+
+function Register() {
+    const styleLink = {
+        textDecoration: 'none',
+        color: '#000000',
+        fontWeight: 'bold'
+    }
+
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+    });
+
+    const inputs = [
+        {
+            id: 1,
+            name: "name",
+            type: "text",
+            placeholder: "Namaku Namamu",
+            label: "Full Name"
+        },
+        {
+            id: 2,
+            name: "email",
+            type: "text",
+            placeholder: "example@gmail.com",
+            label: "Email"
+        },
+        {
+            id: 3,
+            name: "password",
+            type: "password",
+            placeholder: "**********",
+            label: "Password"
+        }
+    ];
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const onChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    return (
+        <Container className="LoginPage" fluid="true">
+            <Form onSubmit={handleSubmit}>
+                <img src={LOGOMYMONEY} className="brand-image" alt="Logo MyMoney" />
+                <div className="title">
+                <h1>Create New Account</h1>
+                <p>Welcome back! Please enter your details</p>
+                </div>
+                {inputs.map((input) => (
+                    <FormInput
+                        key={input.id}
+                        {...input}
+                        value={values[input.name]}
+                        onChange={onChange}
+                    />
+                ))}
+
+                <Button>Create Account</Button>
+                
+                <div className="footer">
+                <p>Dont have an account? <Link style={styleLink} to="/">Sign up for free</Link></p>
+                </div>
+            </Form>
+
+        </Container>
+
+    )
+}
+
+export default Register;
