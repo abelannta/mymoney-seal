b91d70b1db9d475dee535908b8d0f7a75e087266
himma_fl <himma29@gmail.com>
2022-08-08 23:40:25 +0700

    rename typo on dashboard page, move dashboard page file to pages folder, add poppins font in app.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/login/login';
+import DashboarPage from "./pages/dashboard/dashboard-page";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<DashboarPage />} />
       </Routes>
     </div>
   );
