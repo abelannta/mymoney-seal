2127171975cc8a864eedbcc0ff194d0867adbf97
Graha Yohanes Napitupulu <luftschols@gmail.com>
2022-08-17 14:38:03 +0700

    fix page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,13 +2,19 @@ import React from "react";
 import NavbarHome from "../../component/home/navbar/navbar-home";
 import Content from "../../component/home/content/content"
 import Footer from "../../component/home/footer/footer"
+import "../home/home.css"
 
 function Home(){
     return(
         <>
+        <div className="body">
         <NavbarHome />
         <Content />
+        <div className="backgroundAdd">
+
+        </div>
         <Footer />
+        </div>
         </>
         
 
